b00266f3bd74632642f61ff1f9243d521e642e71
Ben Lund <ben@benlund.co.uk>
2011-03-10 17:57:48 -0800

    added flaot object and test

--- lib/roc/objects/float.rb
+++ lib/roc/objects/float.rb
@@ -1,6 +1,28 @@
 require 'roc/objects/base'
 module ROC
   class Float < Base
+    include ROC::Types::ScalarType
+
+    delegate_methods :on => 0.0, :to => :value
+
+    def to_float
+      self.value.to_f
+    end
+    alias to_f to_float
+
+    ## implementing scalar type required methods ##
+
+    def serialize(val)
+      val.to_s
+    end
+    
+    def deserialize(val)
+      if val.nil?
+        nil
+      else
+        val.to_f
+      end
+    end
 
   end
 end
--- test/float_test.rb
+++ test/float_test.rb
@@ -1,9 +1,10 @@
-require 'test/roc_test'
+require File.join(File.dirname(__FILE__), 'roc_test')
 
 class FloatTest < ROCTest
 
   def test_rw
-    f = collection.init_float(random_key)
+    f = Store.init_float(random_key)
+    assert_nil f.value
     assert_equal 0.0, f.to_f
     assert_equal 0, f.to_i
     f.value = 2.345
@@ -11,7 +12,7 @@ class FloatTest < ROCTest
   end
 
   def test_delegation
-    f = collection.init_float(random_key)
+    f = Store.init_float(random_key)
     num = 2.345
     f.value = num
     assert_equal (num - 1.0), (f - 1.0)
